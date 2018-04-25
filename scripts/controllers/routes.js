1263ecf16fa2d16025eb6b96b7aecf83313c2f86
Ben Hurst <BHurst8@gmail.com>
2018-04-25 12:15:42 -0700

    changed even more paths

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,7 +1,7 @@
 
 
-page( '/', () => app.Book.fetchAll(app.booksView.initIndexPage) );
-page( '/book/:id',  ctx => app.booksView.initBookPage(ctx) );
-page( '/add', ctx => app.booksView.initAddPage(ctx) );
+page( '/book-list-client/', () => app.Book.fetchAll(app.booksView.initIndexPage) );
+page( '/book-list-client/book/:id',  ctx => app.booksView.initBookPage(ctx) );
+page( '/book-list-client/add', ctx => app.booksView.initAddPage(ctx) );
 
 page();
